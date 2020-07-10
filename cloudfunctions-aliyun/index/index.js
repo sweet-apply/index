b333533b5d8cd11c4f358e6b65eb8a534066ddf0
夏莹 <ying.xia@yeepay.com>
2020-07-10 15:46:27 +0800

    删除文件

--- cloudfunctions-aliyun/index/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-exports.main = async (event, context) => {
-	//event为客户端上传的参数
-	const res = await uniCloud.httpclient.request('http://39.106.124.138:18080/app/city/cityList', {
-		method: 'GET',
-		dataType: 'json'
-	});
-	return res.data;
-};
